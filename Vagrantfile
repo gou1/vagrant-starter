023e12b613a08aaf2f5eead19afa2bd7f79472b9
Maxime Raoust <max@nadeo.com>
2015-06-18 18:38:13 +0200

    Very seriousness

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,8 @@
 
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "ubuntu/trusty32"
-  config.vm.network "forwarded_port", guest: 80, host: 80
+  config.vm.box = "ubuntu/trusty64"
+  # config.vm.network "forwarded_port", guest: 80, host: 80
 
   config.vm.provision "shell", privileged: false, inline: <<-SHELL
       sudo apt-get update -y --fix-missing
--- Vagrantprovision.sh
+++ Vagrantprovision.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo apt-get -y install apache2
+# Serious business tools
+sudo apt-get install -y fortune figlet toilet cowsay
+
+# I tastes rainbow
+sudo apt-get install -y ruby-full
+sudo gem install lolcat
+
+# Lolcatized-random-cowsayed-fortuned-motd
+sudo sh -c "echo '#!/bin/bash' > /etc/update-motd.d/99-cow"
+sudo sh -c "echo 'echo' >> /etc/update-motd.d/99-cow"
+sudo sh -c "echo '/usr/games/fortune | /usr/games/cowsay -f `ls -1 /usr/share/cowsay/cows/ | sort -R | head -1` -n | lolcat -f' >> /etc/update-motd.d/99-cow"
+sudo chmod a+x /etc/update-motd.d/99-cow
+sudo sh -c 'run-parts /etc/update-motd.d/ > /var/run/motd.dynamic'
